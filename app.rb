a700520f755701422a6184066b3f8e16bd895ad8
steve-alex <steve.alex@hotmail.co.uk>
2019-09-16 17:54:59 +0100

    Done.

--- app.rb
+++ app.rb
@@ -10,7 +10,8 @@ class App < Sinatra::Base
   end
 
   get '/friends' do
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
     # Write your code here!
-
+    erb :friends
   end
 end
